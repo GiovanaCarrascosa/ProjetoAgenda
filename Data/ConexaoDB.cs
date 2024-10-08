c559d7bb38cf598583118386054e86cfb1d36821
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2024-10-08 17:24:57 -0300

    add pasta de banco de dados

---
+++ Data/ConexaoDB.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.Data
+{
+    static public class ConexaoDB
+    {
+
+        static public MySqlConnection CriarConexao()
+        {
+            //string contendo as informações para conexão
+            string stringConexao = "Server=127.0.0.1; Database=dbAgenda;User ID=root;Password=root;";
+
+            //criando a conexão
+            MySqlConnection conexao = new MySqlConnection(stringConexao);
+
+            return conexao;
+
+             
+        }
+    }
+}
--- Views/frm_tela_cadastro.cs
+++ Views/frm_tela_cadastro.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -55,6 +57,37 @@ namespace ProjetoAgenda
         private void txt_senha22_TextChanged(object sender, EventArgs e)
         {
             habilitarBotaoCadastro();
+        }
+
+        private void btn_cadastrar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+            //abrindo conexao
+            conexao.Open();
+
+            //criando o comando SQL para inserir o usuário 
+
+            string sql = $"INSERT INTO tbusuarios(nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha";
+                
+            //criando o comando
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            comando.Parameters.AddWithValue("@nome", txt_nome.Text);
+            comando.Parameters.AddWithValue("@usuario", txt_usuario2.Text);
+            comando.Parameters.AddWithValue("@telefone", txt_telefone.Text);
+            comando.Parameters.AddWithValue("@senha", txt_senha2.Text);
+
+            //executando a instrução SQL no banco
+            comando.ExecuteNonQuery();
+
+            //fechando a conexao com o banco
+            conexao.Close();
+
+            MessageBox.Show("Cadastro efetudo com sucesso! \n Você já pode realizar o login");
+            this.Close();
+
+
         }
     }
 }
