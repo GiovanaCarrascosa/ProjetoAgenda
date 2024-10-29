a9b8920bca760c01bdc60fd376905345e5dd87f9
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2024-10-29 11:29:52 -0300

    feito metade da classe controller

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.Controller
+{
+    internal class UsuarioController
+    {
+        public bool AddUsuario(string nome, string usuario string telefone string senha)
+        {
+            MySqlConnection conxao = ConexaoDB.CriarConexao();
+        }
+    }
+}
