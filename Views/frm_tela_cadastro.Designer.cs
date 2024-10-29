e886816a606e075337d128e7ffee0fb0a0a2f170
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2024-10-29 08:43:11 -0300

    arrumei algumas caixas de texto

--- Views/frm_tela_cadastro.Designer.cs
+++ Views/frm_tela_cadastro.Designer.cs
@@ -51,19 +51,21 @@
             label6.AutoSize = true;
             label6.BackColor = Color.MediumTurquoise;
             label6.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(27, 21);
+            label6.Location = new Point(22, 17);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(118, 34);
+            label6.Size = new Size(102, 29);
             label6.TabIndex = 11;
             label6.Text = "NOME:";
             // 
             // txt_nome
             // 
             txt_nome.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_nome.Location = new Point(27, 58);
+            txt_nome.Location = new Point(22, 46);
+            txt_nome.Margin = new Padding(2);
             txt_nome.Multiline = true;
             txt_nome.Name = "txt_nome";
-            txt_nome.Size = new Size(415, 60);
+            txt_nome.Size = new Size(333, 49);
             txt_nome.TabIndex = 10;
             txt_nome.TextAlign = HorizontalAlignment.Center;
             txt_nome.TextChanged += textBox6_TextChanged;
@@ -73,9 +75,10 @@
             label1.AutoSize = true;
             label1.BackColor = Color.MediumTurquoise;
             label1.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(27, 130);
+            label1.Location = new Point(22, 104);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(170, 34);
+            label1.Size = new Size(145, 29);
             label1.TabIndex = 13;
             label1.Text = "USUÁRIO:";
             // 
@@ -84,9 +87,10 @@
             label7.AutoSize = true;
             label7.BackColor = Color.MediumTurquoise;
             label7.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label7.Location = new Point(27, 239);
+            label7.Location = new Point(22, 191);
+            label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(201, 34);
+            label7.Size = new Size(172, 29);
             label7.TabIndex = 15;
             label7.Text = "TELEFONE:";
             // 
@@ -95,9 +99,10 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Cyan;
             label2.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(27, 500);
+            label2.Location = new Point(22, 400);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(306, 34);
+            label2.Size = new Size(263, 29);
             label2.TabIndex = 17;
             label2.Text = "REPITA A SENHA:";
             label2.TextAlign = ContentAlignment.TopCenter;
@@ -107,9 +112,10 @@
             label3.AutoSize = true;
             label3.BackColor = Color.Cyan;
             label3.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(27, 390);
+            label3.Location = new Point(22, 312);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(137, 34);
+            label3.Size = new Size(118, 29);
             label3.TabIndex = 19;
             label3.Text = "SENHA:";
             // 
@@ -118,19 +124,21 @@
             pictureBox3.BackColor = Color.Transparent;
             pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
             pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox3.Location = new Point(475, 273);
+            pictureBox3.Location = new Point(380, 218);
+            pictureBox3.Margin = new Padding(2);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(257, 254);
+            pictureBox3.Size = new Size(206, 203);
             pictureBox3.TabIndex = 22;
             pictureBox3.TabStop = false;
             // 
             // txt_usuario2
             // 
             txt_usuario2.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_usuario2.Location = new Point(27, 167);
+            txt_usuario2.Location = new Point(22, 134);
+            txt_usuario2.Margin = new Padding(2);
             txt_usuario2.Multiline = true;
             txt_usuario2.Name = "txt_usuario2";
-            txt_usuario2.Size = new Size(415, 60);
+            txt_usuario2.Size = new Size(333, 49);
             txt_usuario2.TabIndex = 23;
             txt_usuario2.TextAlign = HorizontalAlignment.Center;
             txt_usuario2.TextChanged += txt_usuario2_TextChanged;
@@ -138,10 +146,11 @@
             // txt_telefone
             // 
             txt_telefone.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_telefone.Location = new Point(27, 276);
+            txt_telefone.Location = new Point(22, 221);
+            txt_telefone.Margin = new Padding(2);
             txt_telefone.Multiline = true;
             txt_telefone.Name = "txt_telefone";
-            txt_telefone.Size = new Size(415, 60);
+            txt_telefone.Size = new Size(333, 49);
             txt_telefone.TabIndex = 24;
             txt_telefone.TextAlign = HorizontalAlignment.Center;
             txt_telefone.TextChanged += txt_telefone_TextChanged;
@@ -149,10 +158,12 @@
             // txt_senha2
             // 
             txt_senha2.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_senha2.Location = new Point(27, 427);
+            txt_senha2.Location = new Point(22, 342);
+            txt_senha2.Margin = new Padding(2);
             txt_senha2.Multiline = true;
             txt_senha2.Name = "txt_senha2";
-            txt_senha2.Size = new Size(415, 60);
+            txt_senha2.PasswordChar = '*';
+            txt_senha2.Size = new Size(333, 49);
             txt_senha2.TabIndex = 25;
             txt_senha2.TextAlign = HorizontalAlignment.Center;
             txt_senha2.TextChanged += txt_senha2_TextChanged;
@@ -160,10 +171,12 @@
             // txt_senha22
             // 
             txt_senha22.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_senha22.Location = new Point(27, 537);
+            txt_senha22.Location = new Point(22, 430);
+            txt_senha22.Margin = new Padding(2);
             txt_senha22.Multiline = true;
             txt_senha22.Name = "txt_senha22";
-            txt_senha22.Size = new Size(415, 60);
+            txt_senha22.PasswordChar = '*';
+            txt_senha22.Size = new Size(333, 49);
             txt_senha22.TabIndex = 26;
             txt_senha22.TextAlign = HorizontalAlignment.Center;
             txt_senha22.TextChanged += txt_senha22_TextChanged;
@@ -173,9 +186,10 @@
             btn_cadastrar.BackColor = Color.PaleTurquoise;
             btn_cadastrar.Enabled = false;
             btn_cadastrar.Font = new Font("Sitka Subheading", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_cadastrar.Location = new Point(499, 533);
+            btn_cadastrar.Location = new Point(389, 430);
+            btn_cadastrar.Margin = new Padding(2);
             btn_cadastrar.Name = "btn_cadastrar";
-            btn_cadastrar.Size = new Size(213, 64);
+            btn_cadastrar.Size = new Size(185, 51);
             btn_cadastrar.TabIndex = 27;
             btn_cadastrar.Text = "CADASTRAR:";
             btn_cadastrar.UseVisualStyleBackColor = false;
@@ -186,18 +200,19 @@
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(475, 35);
+            pictureBox1.Location = new Point(380, 28);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(257, 254);
+            pictureBox1.Size = new Size(206, 203);
             pictureBox1.TabIndex = 28;
             pictureBox1.TabStop = false;
             // 
             // frm_tela_cadastro
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkCyan;
-            ClientSize = new Size(744, 621);
+            ClientSize = new Size(595, 497);
             Controls.Add(pictureBox1);
             Controls.Add(btn_cadastrar);
             Controls.Add(txt_senha22);
@@ -212,6 +227,7 @@
             Controls.Add(label6);
             Controls.Add(txt_nome);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
             Name = "frm_tela_cadastro";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tela de Cadastro";
--- Views/frm_tela_login.Designer.cs
+++ Views/frm_tela_login.Designer.cs
@@ -44,9 +44,10 @@
             btn_entrar.BackColor = Color.PaleTurquoise;
             btn_entrar.Enabled = false;
             btn_entrar.Font = new Font("Sitka Subheading", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_entrar.Location = new Point(148, 298);
+            btn_entrar.Location = new Point(118, 238);
+            btn_entrar.Margin = new Padding(2);
             btn_entrar.Name = "btn_entrar";
-            btn_entrar.Size = new Size(163, 61);
+            btn_entrar.Size = new Size(137, 49);
             btn_entrar.TabIndex = 0;
             btn_entrar.Text = "ENTRAR";
             btn_entrar.UseVisualStyleBackColor = false;
@@ -56,9 +57,10 @@
             label1.AutoSize = true;
             label1.BackColor = Color.MediumTurquoise;
             label1.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(24, 24);
+            label1.Location = new Point(19, 19);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(170, 34);
+            label1.Size = new Size(145, 29);
             label1.TabIndex = 5;
             label1.Text = "USUÁRIO:";
             // 
@@ -66,9 +68,10 @@
             // 
             pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
             pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
-            pictureBox1.Location = new Point(480, -34);
+            pictureBox1.Location = new Point(384, -27);
+            pictureBox1.Margin = new Padding(2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(568, 578);
+            pictureBox1.Size = new Size(454, 462);
             pictureBox1.TabIndex = 7;
             pictureBox1.TabStop = false;
             // 
@@ -77,9 +80,10 @@
             label2.AutoSize = true;
             label2.BackColor = Color.MediumTurquoise;
             label2.Font = new Font("Modern No. 20", 15.9999981F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(24, 157);
+            label2.Location = new Point(19, 126);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(137, 34);
+            label2.Size = new Size(118, 29);
             label2.TabIndex = 8;
             label2.Text = "SENHA:";
             // 
@@ -87,9 +91,10 @@
             // 
             btn_cadastrar.BackColor = Color.PaleTurquoise;
             btn_cadastrar.Font = new Font("Sitka Subheading", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_cadastrar.Location = new Point(124, 378);
+            btn_cadastrar.Location = new Point(94, 305);
+            btn_cadastrar.Margin = new Padding(2);
             btn_cadastrar.Name = "btn_cadastrar";
-            btn_cadastrar.Size = new Size(213, 64);
+            btn_cadastrar.Size = new Size(181, 51);
             btn_cadastrar.TabIndex = 10;
             btn_cadastrar.Text = "CADASTRAR:";
             btn_cadastrar.UseVisualStyleBackColor = false;
@@ -98,10 +103,11 @@
             // txt_usuario
             // 
             txt_usuario.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_usuario.Location = new Point(24, 76);
+            txt_usuario.Location = new Point(19, 61);
+            txt_usuario.Margin = new Padding(2);
             txt_usuario.Multiline = true;
             txt_usuario.Name = "txt_usuario";
-            txt_usuario.Size = new Size(415, 60);
+            txt_usuario.Size = new Size(333, 49);
             txt_usuario.TabIndex = 11;
             txt_usuario.TextAlign = HorizontalAlignment.Center;
             txt_usuario.TextChanged += textBox6_TextChanged;
@@ -109,21 +115,22 @@
             // txt_senha
             // 
             txt_senha.Font = new Font("Leelawadee UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txt_senha.Location = new Point(24, 207);
+            txt_senha.Location = new Point(19, 166);
+            txt_senha.Margin = new Padding(2);
             txt_senha.Multiline = true;
             txt_senha.Name = "txt_senha";
             txt_senha.PasswordChar = '*';
-            txt_senha.Size = new Size(415, 60);
+            txt_senha.Size = new Size(333, 49);
             txt_senha.TabIndex = 12;
             txt_senha.TextAlign = HorizontalAlignment.Center;
             txt_senha.TextChanged += txt_senha_TextChanged;
             // 
             // frm_tela_login
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkCyan;
-            ClientSize = new Size(1039, 503);
+            ClientSize = new Size(831, 402);
             Controls.Add(txt_senha);
             Controls.Add(txt_usuario);
             Controls.Add(btn_cadastrar);
@@ -133,6 +140,7 @@
             Controls.Add(btn_entrar);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(2);
             Name = "frm_tela_login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tela de Login";
