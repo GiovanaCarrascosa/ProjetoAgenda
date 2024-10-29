75296bdb6c445f769c7c91a70eeaa43be5419c98
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2024-10-29 15:00:49 -0300

    add finalização banco de dado

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -10,9 +10,41 @@ namespace ProjetoAgenda.Controller
 {
     internal class UsuarioController
     {
-        public bool AddUsuario(string nome, string usuario string telefone string senha)
+        public bool AddUsuario(string nome, string usuario, string telefone, string senha)
         {
-            MySqlConnection conxao = ConexaoDB.CriarConexao();
+            //cria a conexao, estou utilzando a classe ConexaoDB q esta dentro da pasta DATA
+            MySqlConnection conexao = ConexaoDB.CriarConexao();
+
+            //comando sql que sera executado
+            string sql = "INSERT INTO tbUsuarios (nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha);";
+
+            //abri a conexao com o banco
+            conexao.Open();
+
+            //esse cara é o responsavel por executar o comando sql
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            //estou trocando o valor dos @ pelas informações que serao cadastradas
+            //essas informações vieram dos parametros da função
+            comando.Parameters.AddWithValue("@nome", nome);
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            comando.Parameters.AddWithValue("@telefone", telefone);
+            comando.Parameters.AddWithValue("@senha", senha);
+
+            //executando no banco de dados
+            int linhasAfetadas = comando.ExecuteNonQuery();
+
+            conexao.Close();
+
+            if (linhasAfetadas > 0) 
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
         }
     }
 }
