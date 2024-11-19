df330bc036c5787f6957238b879825d5e70c7a52
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2024-11-19 11:34:41 -0300

    att o agendaDB - anotações

--- estruturaDB/agendaDB.sql
+++ estruturaDB/agendaDB.sql
@@ -15,3 +15,25 @@ create table categorias(
 
 	cod_categoria int not null auto_increment primary key,
     categoria varchar (50) not null);
+
+
+create table categorias(
+
+cod_categoria int auto_increment primary key not null,
+categoria varchar (80) not null,
+usuario varchar (20)
+);
+
+delimiter $$
+
+create trigger trInsertCategoria
+	before
+    insert
+    on categorias
+    for each row
+begin
+	set new.usuario = CURRENT_USR();
+end;
+$$
+
+delimiter ;
